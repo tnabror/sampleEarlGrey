33d84632b9e02defd9d83bdea2ca5f704763cdf8
Rakesh <>
2018-04-05 17:27:01 +0530

    test:added testPresenceOfKeyWindow

--- sampleEarlGreyTests/sampleEarlGreyTests.swift
+++ sampleEarlGreyTests/sampleEarlGreyTests.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2018 Rakesh. All rights reserved.
 //
 
+import EarlGrey
 import XCTest
 
 class sampleEarlGreyTests: XCTestCase {
@@ -19,12 +20,11 @@ class sampleEarlGreyTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
+
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testPresenceOfKeyWindow() {
+        EarlGrey.selectElement(with: grey_keyWindow()).assert(grey_sufficientlyVisible())
     }
-    
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measure {
